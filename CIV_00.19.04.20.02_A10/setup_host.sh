1bc7d9dd2986910ae79f61285b12597eeb3b4002
swaroopb <swaroop.balan@intel.com>
2020-05-24 20:22:45 +0530

    Modified old releases so that it can be reproduced
    Signed-off-by: swaroopb <swaroop.balan@intel.com>

--- CIV_00.19.04.20.02_A10/setup_host.sh
+++ CIV_00.19.04.20.02_A10/setup_host.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 reboot_required=0
-QEMU_REL=qemu-4.2.0
-a=`grep -rn CIV_WORK_DIR /etc/environment`
 
 function ubu_changes_require(){
 	echo "Please make sure your apt is working"
@@ -11,17 +9,25 @@ function ubu_changes_require(){
 	if [ x$res = xn ]; then
 		exit 0
 	fi
-	apt install -y wget mtools ovmf dmidecode python3-usb python3-pyudev
+	apt install -y wget mtools ovmf
 }
 
 function ubu_install_qemu(){
 	apt purge -y "qemu*"
 	apt autoremove -y
-	apt install -y git libfdt-dev libpixman-1-dev libssl-dev vim socat libsdl2-dev libspice-server-dev autoconf libtool xtightvncviewer tightvncserver x11vnc uuid-runtime uuid uml-utilities bridge-utils python-dev liblzma-dev libc6-dev libegl1-mesa-dev libepoxy-dev libdrm-dev libgbm-dev libaio-dev libusb-1.0.0-dev libgtk-3-dev bison libcap-dev libattr1-dev
+	apt install -y git libfdt-dev libpixman-1-dev libssl-dev vim socat libsdl1.2-dev libspice-server-dev autoconf libtool xtightvncviewer tightvncserver x11vnc libsdl1.2-dev uuid-runtime uuid uml-utilities bridge-utils python-dev liblzma-dev libc6-dev libegl1-mesa-dev libepoxy-dev libdrm-dev libgbm-dev libaio-dev libusb-1.0.0-dev libgtk-3-dev bison
 
-	wget https://download.qemu.org/$QEMU_REL.tar.xz
-	tar -xf $QEMU_REL.tar.xz
-	cd $QEMU_REL/
+	wget https://download.qemu.org/qemu-3.0.0.tar.xz
+	tar -xf qemu-3.0.0.tar.xz
+	cd qemu-3.0.0/
+	wget https://patchwork.kernel.org/patch/10678791/raw/ -O Audio_fix.patch &&  patch -p1 < Audio_fix.patch
+	if [ $? != "0" ]; then
+		echo "Patch didn't apply"
+		echo "Please check ..."
+		exit 1
+	else
+		echo "Patch applied"
+	fi
 	./configure --prefix=/usr \
 	    --enable-kvm \
 	    --disable-xen \
@@ -34,30 +40,11 @@ function ubu_install_qemu(){
 	    --disable-debug-tcg \
 	    --enable-opengl \
 	    --enable-gtk \
-	    --enable-virtfs \
 	    --target-list=x86_64-softmmu \
 	    --audio-drv-list=pa
 	make -j24
 	make install
 	cd ../
-}
-
-function install_9p_module(){
-	echo "installing 9p kernel modules for file-sharing"
-	sudo modprobe 9pnet
-	sudo modprobe 9pnet_virtio
-	sudo modprobe 9p
-	mkdir ./share_folder
-}
-
-function ubu_build_ovmf(){
-	sudo apt install -y uuid-dev nasm acpidump iasl
-	cd $QEMU_REL/roms/edk2
-	source ./edksetup.sh
-	make -C BaseTools/
-	build -b DEBUG -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc -D NETWORK_IP4_ENABLE -D NETWORK_ENABLE  -D SECURE_BOOT_ENABLE -DTPM2_ENABLE=TRUE
-	cp Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd ../../../OVMF.fd
-	cd ../../../
 }
 
 function ubu_enable_host_gvtg(){
@@ -68,10 +55,6 @@ function ubu_enable_host_gvtg(){
 		fi
 		sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"i915.enable_gvt=1 intel_iommu=on /g" /etc/default/grub
 		update-grub
-
-		echo -e "\nkvmgt\nvfio-iommu-type1\nvfio-mdev\n" >> /etc/initramfs-tools/modules
-		update-initramfs -u -k all
-
 		reboot_required=1
 	fi
 }
@@ -132,21 +115,8 @@ function clr_enable_host_gvtg(){
 function get_required_scripts(){
 	#wget https://raw.githubusercontent.com/projectceladon/device-androidia-mixins/master/groups/device-specific/caas/start_flash_usb.sh
 	#wget https://raw.githubusercontent.com/projectceladon/device-androidia-mixins/master/groups/device-specific/caas/start_android_qcow2.sh
-	#wget https://raw.githubusercontent.com/projectceladon/device-androidia-mixins/master/groups/device-specific/caas/auto_switch_pt_usb_vms.sh
-	#wget https://raw.githubusercontent.com/projectceladon/device-androidia-mixins/master/groups/device-specific/caas/findall.py
-	chmod +x auto_switch_pt_usb_vms.sh
-	chmod +x findall.py
 	chmod +x start_flash_usb.sh
 	chmod +x start_android_qcow2.sh
-}
-
-function save_env(){
-	if [ -z "$a" ]; then
-		echo "export CIV_WORK_DIR=$(pwd)" | tee -a /etc/environment
-	else
-		sed -i "s|export CIV_WORK_DIR.*||g" /etc/environment
-		echo "export CIV_WORK_DIR=$(pwd)" | tee -a /etc/environment
-	fi
 }
 
 version=`cat /proc/version`
@@ -155,12 +125,9 @@ if [[ $version =~ "Ubuntu" ]]; then
 	check_network
 	ubu_changes_require
 	ubu_install_qemu
-	ubu_build_ovmf
 	ubu_enable_host_gvtg
 	get_required_scripts
 	check_kernel
-	save_env
-	install_9p_module
 	ask_reboot
 elif [[ $version =~ "Clear Linux OS" ]]; then
 	check_network
@@ -168,8 +135,6 @@ elif [[ $version =~ "Clear Linux OS" ]]; then
 	clr_enable_host_gvtg
 	get_required_scripts
 	check_kernel
-	save_env
-	install_9p_module
 	ask_reboot
 else
 	echo "only clearlinux or Ubuntu is supported"
---
+++ CIV_00.19.04.51_A10/start_android_qcow2.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+work_dir=$PWD
+caas_image=$work_dir/android.qcow2
+
+ovmf_file="./OVMF.fd"
+[ ! -f $ovmf_file ] && ovmf_file="/usr/share/qemu/OVMF.fd"
+
+GVTg_DEV_PATH="/sys/bus/pci/devices/0000:00:02.0"
+GVTg_VGPU_UUID="4ec1ff92-81d7-11e9-aed4-5bf6a9a2bb0a"
+function setup_vgpu(){
+	res=0
+	if [ ! -d $GVTg_DEV_PATH/$GVTg_VGPU_UUID ]; then
+		echo "Creating VGPU..."
+		sudo sh -c "echo $GVTg_VGPU_UUID > $GVTg_DEV_PATH/mdev_supported_types/i915-GVTg_V5_8/create"
+		res=$?
+	fi
+	return $res
+}
+
+common_options="\
+ -m 2048 -smp 2 -M q35 \
+ -name caas-vm \
+ -enable-kvm \
+ -vga none \
+ -display gtk,gl=on \
+ -k en-us \
+ -machine kernel_irqchip=off \
+ -global PIIX4_PM.disable_s3=1 -global PIIX4_PM.disable_s4=1 \
+ -cpu host \
+ -device qemu-xhci,id=xhci,addr=0x8 \
+ -device usb-host,vendorid=0x046d,productid=0x082d \
+ -device usb-host,vendorid=0x046d,productid=0x085c \
+ -device usb-host,vendorid=0x03eb,productid=0x8a6e \
+ -device usb-host,vendorid=0x0eef,productid=0x7200 \
+ -device usb-host,vendorid=0x222a,productid=0x0141 \
+ -device usb-host,vendorid=0x222a,productid=0x0088 \
+ -device usb-host,vendorid=0x8087,productid=0x0a2b \
+ -device usb-mouse \
+ -device usb-kbd \
+ -drive file=$ovmf_file,format=raw,if=pflash \
+ -chardev socket,id=charserial0,path=./kernel-console,server,nowait \
+ -device isa-serial,chardev=charserial0,id=serial0 \
+ -device intel-hda -device hda-duplex \
+ -drive file=$caas_image,if=none,id=disk1 \
+ -device virtio-blk-pci,drive=disk1,bootindex=1 \
+ -device e1000,netdev=net0 \
+ -netdev user,id=net0,hostfwd=tcp::5555-:5555 \
+ -device intel-iommu,device-iotlb=off \
+ -nodefaults
+"
+
+function launch_hwrender(){
+	qemu-system-x86_64 \
+	  -device vfio-pci,sysfsdev=$GVTg_DEV_PATH/$GVTg_VGPU_UUID,display=on,x-igd-opregion=on \
+	  $common_options
+}
+
+function launch_swrender(){
+	qemu-system-x86_64 \
+	  -device qxl-vga,xres=1280,yres=720 \
+	  $common_options
+}
+
+function check_nested_vt(){
+	nested=$(cat /sys/module/kvm_intel/parameters/nested)
+	if [[ $nested != 1 && $nested != 'Y' ]]; then
+		echo "E: Nested VT is not enabled!"
+		exit -1
+	fi
+}
+
+version=`cat /proc/version`
+
+vno=$(echo $version | \
+	awk '{
+		for(i=0;i<NF;i++) { if ($i == "Linux" && $(i+1) == "version") { print $(i+2); next; } }
+	}'
+)
+if [[ "$vno" > "5.0.0" ]]; then
+	check_nested_vt
+	setup_vgpu
+	if [[ $? == 0 ]]; then
+		launch_hwrender
+	else
+		echo "W: Failed to create vgpu, fall to software rendering"
+		launch_swrender
+	fi
+else
+	echo "E: Detected linux version $vno"
+	echo "E: Please upgrade kernel version newer than 5.0.0!"
+	exit -1
+fi
+
